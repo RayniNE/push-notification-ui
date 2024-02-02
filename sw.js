910a38324a9f6b62dd9609341630ccfa36530d79
Rayni Nunez Espino <timetopwm@gmail.com>
2024-02-02 19:36:24 -0400

    Initial commit. Basic website with html and javascript, it includes a service-worker. This is the only way i found to make it work. Frameworks like React, Svelte gave a lot of issues registering the service-worker.

---
+++ sw.js
@@ -0,0 +1,41 @@
+const urlBase64ToUint8Array = base64String => {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+
+    return outputArray;
+}
+
+const saveSubscription = async (subscription) => {
+
+    const response = await fetch('http://127.0.0.1:8080/subscribe', {
+        method: 'POST',
+        headers: { 'Content-type': "application/json" },
+        mode: 'cors',
+        body: JSON.stringify(subscription)
+    })
+
+    return response.json()
+}
+
+self.addEventListener("activate", async (e) => {
+    const subscription = await self.registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array("BNTyUF41b8UXl7htTMPUyH3VJ_bQemJvK2OlSmFT9cLqzCCeVYS5lDPpmQhoze4YLyen6uAG-JCaRyLY66OK5jk")
+    })
+
+    const response = await saveSubscription(subscription)
+    console.log(response)
+})
+
+self.addEventListener("push", e => {
+    self.registration.showNotification("Wohoo!!", { body: e.data.text() })
+})
